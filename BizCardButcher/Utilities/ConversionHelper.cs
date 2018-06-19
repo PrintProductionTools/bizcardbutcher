434fb220888d4994c406c60943bfc3b829cd827a
Daniel Backus <danbackus@gmail.com>
2018-06-19 16:40:18 -0400

    Initial commit

---
+++ BizCardButcher/Utilities/ConversionHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizCardButcher.Utilities
+{
+    class ConversionHelper
+    {
+        internal static float MMToDots(float mm)
+        {
+            float dots = InchesToDots(MMToInches(mm));
+            return dots;
+        }
+
+        internal static float MMToInches(float mm)
+        {
+            float inches = mm / 25.4f;
+            return inches;
+        }
+
+        internal static float InchesToDots(float inches)
+        {
+            float dots = inches * 72f;
+            return dots;
+        }
+    }
+}
